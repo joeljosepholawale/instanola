3a1b91083d53551e5fbbd75aa194adf13e74480c
joel joseph <39407860+joeljosepholawale@users.noreply.github.com>
2025-09-14 03:46:51 +0100

    Fix Nodemailer createTransport Typo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -514,7 +514,7 @@ export const sendNotificationEmail = onCall({
     }
 
     // Configure nodemailer transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: smtpUserEmail,
