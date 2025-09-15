008bb5b69448859b68337b342f9acd79652ac3b2
joel joseph <39407860+joeljosepholawale@users.noreply.github.com>
2025-09-15 16:04:38 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['@babel/plugin-syntax-throw-expressions']
+      }
+    })
+  ],
   build: {
     target: 'esnext',
     minify: 'terser',
